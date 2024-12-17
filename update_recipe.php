dd4f21890e9b035260468d7a9daf9dd4cfa263d9
Sean Gbadamosi <139792459+seangbadamosi@users.noreply.github.com>
2024-12-17 13:05:14 +0100

    added php code

--- update_recipe.php
+++ update_recipe.php
@@ -1,5 +1,33 @@
 <?php
+    include "./connect.php";
 
+    $recipe_id = $_GET ['recipe_id'];
+
+    $fetch_query = "SELECT * FROM `recipes_tb` WHERE `recipe_id` = '$recipe_id'";
+
+    $send_query = mysqli_query($connect, $fetch_query);
+
+    $recipe = mysqli_fetch_assoc($send_query);
+
+    print_r($recipe);
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.css">
+    <title>Document</title>
+</head>
+<body>
+    
+
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.js"></script>
+</body>
+</html>
